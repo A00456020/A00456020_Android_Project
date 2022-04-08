d7c9ceacf0fa78094c20601a2c3ed7a78554323d
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-04-08 19:42:58 -0300

    Created new onViewCreated method to initialize the ReservationDetailsFragment.

--- app/src/main/java/com/example/a00456020_android_project/ReservationDetailsFragment.java
+++ app/src/main/java/com/example/a00456020_android_project/ReservationDetailsFragment.java
@@ -10,18 +10,20 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 public class ReservationDetailsFragment extends Fragment {
 
     View view;
-    EditText customer_name_edit_text;
-    EditText customer_email_edit_text;
-    EditText number_of_rooms_edit_text;
-    EditText number_of_guests_edit_text;
-    DatePicker check_in_date_date_picker;
-    DatePicker check_out_date_date_picker;
-    Button reservation_search_button;
+    ConstraintLayout mainConstraintLayout;
+    EditText customerNameEditText;
+    EditText customerEmailEditText;
+    EditText numberOfRoomsEditText;
+    EditText numberOfGuestsEditText;
+    DatePicker checkInDateDatePicker;
+    DatePicker checkOutDateDatePicker;
+    Button reservationSearchButton;
 
 
     @Nullable
@@ -29,5 +31,34 @@ public class ReservationDetailsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        view = inflater.inflate(R.layout.reservation_details_layout, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mainConstraintLayout = view.findViewById(R.id.main_constraint_layout);
+        customerNameEditText = view.findViewById(R.id.customer_name_edit_text);
+        customerEmailEditText = view.findViewById(R.id.customer_email_edit_text);
+        numberOfRoomsEditText = view.findViewById(R.id.number_of_rooms_edit_text);
+        numberOfGuestsEditText = view.findViewById(R.id.number_of_guests_edit_text);
+        checkInDateDatePicker = view.findViewById(R.id.check_in_date_date_picker);
+        checkOutDateDatePicker = view.findViewById(R.id.check_out_date_date_picker);
+        reservationSearchButton = view.findViewById(R.id.reservation_search_button);
+
+        reservationSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        reservationSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
