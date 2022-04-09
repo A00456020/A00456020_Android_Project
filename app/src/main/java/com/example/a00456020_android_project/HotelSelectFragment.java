f5437fde80fb9ebd9b75e9c1d8deb20643173b75
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-04-08 21:15:13 -0300

    Created onCreateView and onViewCreated Methods for HotelSelectFragment.

--- app/src/main/java/com/example/a00456020_android_project/HotelSelectFragment.java
+++ app/src/main/java/com/example/a00456020_android_project/HotelSelectFragment.java
@@ -1,7 +1,27 @@
 package com.example.a00456020_android_project;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HotelSelectFragment extends Fragment {
 
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.hotel_select_layout, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
