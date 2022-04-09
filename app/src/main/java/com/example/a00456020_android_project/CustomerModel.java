094fc0b03f4d806c47de1c4f68ec5d79a6207687
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-04-08 23:07:46 -0300

    Created Customer model according to sample api response.

---
+++ app/src/main/java/com/example/a00456020_android_project/CustomerModel.java
@@ -0,0 +1,48 @@
+package com.example.a00456020_android_project;
+
+public class CustomerModel {
+    /*
+    {
+        "id": 1,
+        "name": "Parth",
+        "mobile": "9024567898",
+        "email": "vdpar9556@ymail.com"
+    }
+    */
+    String Id;
+    String name;
+    String email;
+    String mobile;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
